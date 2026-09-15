9eb6c5a9fda8cbc14e417bf2cc0f142af9c5b9b4
Uratile <uratilemei@gmail.com>
2026-09-15 23:28:36 +0200

    just a commit

--- IsleWork/Program.cs
+++ IsleWork/Program.cs
@@ -1,3 +1,5 @@
 ﻿using IsleWork;
 
-Employee employee = new Employee(0, "John", "Doe", "employee@gmail.com", "Software Developer" );
+Employee employee = new Employee();
+//Employee employee = new Employee(6, "John", "Doe", "employee@gmail.com", "Software Developer" );
+employee.Print();
